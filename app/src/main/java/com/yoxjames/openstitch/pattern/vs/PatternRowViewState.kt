a31b32350068f485259e672f8f6a3aa6f8f41584
Baghaii <baghaii@gmail.com>
2022-03-13 08:26:04 -0600

    Update rows to grid

--- app/src/main/java/com/yoxjames/openstitch/pattern/vs/PatternRowViewState.kt
+++ app/src/main/java/com/yoxjames/openstitch/pattern/vs/PatternRowViewState.kt
@@ -1,20 +1,19 @@
 package com.yoxjames.openstitch.pattern.vs
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.placeholder.PlaceholderHighlight
 import com.google.accompanist.placeholder.placeholder
@@ -24,6 +23,7 @@ import com.yoxjames.openstitch.core.ViewEventListener
 import com.yoxjames.openstitch.list.Click
 import com.yoxjames.openstitch.list.ListItemViewEvent
 import com.yoxjames.openstitch.list.ListItemViewState
+import com.yoxjames.openstitch.ui.theme.Teal200
 import kotlinx.coroutines.launch
 
 data class PatternRowViewState(
@@ -43,19 +43,25 @@ data class PatternRowViewState(
             elevation = 4.dp,
             onClick = { coroutineScope.launch { viewEventListener.onEvent(Click) } }
         ) {
-            Row(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.fillMaxWidth()) {
                 GlideImage(
                     modifier = Modifier
-                        .height(100.dp)
-                        .width(100.dp)
-                        .padding(8.dp)
+                        .height(140.dp)
+                        .fillMaxWidth()
+                        .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                        .align(CenterHorizontally)
                         .patternPlaceholder(),
                     imageModel = imageUrl,
-                    contentScale = ContentScale.Crop
+                    contentScale = ContentScale.Crop,
                 )
-                Column(modifier = Modifier.padding(8.dp)) {
-                    Text(modifier = Modifier.patternPlaceholder(), text = name, fontWeight = FontWeight.Bold)
-                    Text(modifier = Modifier.patternPlaceholder(), text = "by $author")
+                Column(modifier = Modifier
+                    .align(CenterHorizontally)
+                    .fillMaxWidth()
+                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                    .patternPlaceholder()
+                    .background(color = Teal200)) {
+                    Text(modifier = Modifier.padding(start = 8.dp, end = 8.dp, top = 8.dp), text = name, fontWeight = FontWeight.Bold, maxLines = 1, overflow = TextOverflow.Ellipsis)
+                    Text(modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 8.dp), text = "by $author", maxLines = 1, overflow = TextOverflow.Ellipsis)
                 }
             }
         }
